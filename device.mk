7824b7610906bd101f52a9cda0d94287a4f9fed5
TheStrechh <17030540@upgto.edu.mx>
2018-03-28 15:51:47 +0000

    kenzo: update overlays

--- device.mk
+++ device.mk
@@ -17,6 +17,7 @@
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
 
 # call the proprietary setup
 $(call inherit-product-if-exists, vendor/xiaomi/kenzo/kenzo-vendor.mk)
